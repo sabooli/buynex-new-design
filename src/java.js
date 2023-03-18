209dd999eccf834cdc1ef4478c3b3c3c95c6806d
Saba <sabooli22@gmail.com>
2023-03-18 21:45:12 +0330

    made header ,slider, footer of Home

---
+++ src/java.js
@@ -0,0 +1,24 @@
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  centeredSlides: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
