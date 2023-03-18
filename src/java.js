498d69fe3a2f0e908090945e500bb452a131d244
Saba <sabooli22@gmail.com>
2023-03-19 01:58:11 +0330

    made read more button work finally

--- src/java.js
+++ src/java.js
@@ -1,24 +1,21 @@
 var swiper = new Swiper(".slide-content", {
   slidesPerView: 4,
   spaceBetween: 30,
-  centeredSlides: true,
+  centeredSlides: false,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
 });
-function myFunction() {
-  var dots = document.getElementById("dots");
-  var moreText = document.getElementById("more");
-  var btnText = document.getElementById("myBtn");
 
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnText.innerHTML = "Read more";
-    moreText.style.display = "none";
-  } else {
-    dots.style.display = "none";
-    btnText.innerHTML = "Read less";
-    moreText.style.display = "inline";
-  }
-}
+$(document).ready(function () {
+  $(".read").click(function () {
+    $(this).prev().toggle();
+    $(this).siblings(".dots").toggle();
+    if ($(this).text() == "Read more") {
+      $(this).text("Read less");
+    } else {
+      $(this).text("Read more");
+    }
+  });
+});
