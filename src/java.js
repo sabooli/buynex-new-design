a255c4de4c04f7ffae327a0bbc9dba2d728052c3
sabooli <109861969+sabooli@users.noreply.github.com>
2023-05-07 23:48:20 +0330

    Update java.js

--- src/java.js
+++ src/java.js
@@ -2,6 +2,10 @@ var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
   centerSlides: "true",
   grabCursor: "true",
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
